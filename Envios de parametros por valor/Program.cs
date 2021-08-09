67aced88399e181d8e3395cacc1a54b730dd9e5a
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2021-08-09 00:59:19 -0700

    Proyecto
    Conocimentos basicos.

---
+++ Envios de parametros por valor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Envios_de_parametros_por_valor
+{
+    class Program
+    {
+        static int x = 5; //Varible global
+
+        static void Main(string[] args)
+        {
+            int y = 13;//variable local
+            Console.WriteLine("\nx=" + x);
+
+            //Llamada al metodo y envio por valor 
+
+            Metodo(y);
+            Console.WriteLine("\nx=" + x);
+            Console.WriteLine("\ny=" + y);
+            Console.ReadLine();
+        }
+
+        //El parametro "a" recibe el valor de "y"
+        static void Metodo(int a)
+        {
+            a = a + 3;
+            Console.WriteLine("\na=" + a);
+            x = x * 2;
+        }
+    }
+}
